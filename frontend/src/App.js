241fb5f08a8bf5321052f4e3d721f85860b5cc15
Abhigyan <abhi2018aray@gmail.com>
2024-04-05 02:56:04 +0530

    first commit for react fronted, completed till doctor login and registe frontend page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
-import Encrypt from "./components/Encrypt";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import DoctorLogin from "./pages/Doctor/DoctorLogin";
+import DoctorRegister from "./pages/Doctor/DoctorRegister";
 
 function App() {
   return (
-    <div>
-      <p>hi</p>
-      <Encrypt />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctorLogin" element={<DoctorLogin />} />
+        <Route path="/doctorRegister" element={<DoctorRegister />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Patient/PatientLogin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PatientLogin() {
+  return (
+    <div>
+      <p>Patient Login page</p>
+    </div>
+  );
+}
+
+export default PatientLogin;
